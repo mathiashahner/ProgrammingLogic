ddda62613c7feae08ff046e6ecb7b50224c343b0
Mathias Hahner <mathias_hahner@hotmail.com>
2021-03-29 23:14:23 -0300

    Resolve exercise 002

--- ex002/ex002.c
+++ ex002/ex002.c
@@ -1,38 +1,54 @@
+/*
+    Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+
+    Example:
+        - For a = [2, 1, 3, 5, 3, 2], the output should be firstDuplicate(a) = 3.
+        - There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+        - For a = [2, 2], the output should be firstDuplicate(a) = 2;
+        - For a = [2, 4, 3, 5, 1], the output should be firstDuplicate(a) = -1.
+*/
+
 #include <stdio.h>
 
-int firstDuplicate( int * a );
+int firstDuplicate(int a[], int tam);
 
 int main()
 {
-    int a[] = {2,6,8,2,4};
+    int a[] = {2,6,8,2,4,4};
+    int tam = sizeof(a) / sizeof(a[0] - 1);
 
-    printf("%d\n", firstDuplicate(a));
+    system("cls");
+    printf("%d\n\n", firstDuplicate(a, tam));
 
     return 0;
 }
 
-int firstDuplicate( int * a )
+int firstDuplicate(int a[], int tam)
 {
-    int tam, result;
-
-    tam = sizeof(a)/sizeof(int);
-
-    for( int i=0; i<tam; i++ )
+    int count_1, count_2, index, result;
+    
+    result = -1;
+    index = tam + 1;
+    
+    for(count_1=0; count_1<tam; count_1++)
     {
-        result = a[i];
-
-        for( int j=0; j<tam; j++ )
+        for(count_2=0; count_2<tam; count_2++)
         {
-            if( result == a[j] )
-            {
-                result = a[j];
-            }
-            else
-            {
-                result = -1;
-            }
-        }
+            if((a[count_1] == a[count_2]) && (count_1 != count_2))
+            {                
+                if((count_2 >= count_1) && ((count_2 - count_1) < index))
+                {
+                    result = a[count_2];
+                    index = count_2 - count_1;
+                }
+                else if((count_2 < count_1) && ((count_1 - count_2) < index))
+                {
+                    result = a[count_2];
+                    index = count_1 - count_2;
+                }
+            }                                         
+            //printf("count_1=[%d] count_2=[%d] result=[%d] index=[%d]\n", count_1, count_2, result, index );
+        }        
     }
-
     return result;
 }
